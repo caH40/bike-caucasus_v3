a5c8e535905412fff7c55ff8f720634f4197534a
Alexander Berezhnev <freelancerbav@gmail.com>
2024-05-03 22:09:37 +0300

    v3.4.3 Linking provider to accaunt during lodin

---
+++ src/app/auth/login/access-denied/page.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+import AuthBlock from '@/components/UI/AuthBlock/AuthBlock';
+import styles from './access-denied.module.css';
+
+export default function AccessDenied() {
+  return (
+    <AuthBlock>
+      <h1 className={styles.title}>Отказ в доступе</h1>
+      <div>
+        <ol className={styles.list}>
+          <li>Сначала зарегистрируйтесь на сайте, введя свои данные.</li>
+          <li>После регистрации привяжите yandexId, vkId или googlId для быстрого входа.</li>
+          <li>
+            Теперь вы можете использовать этот провайдер для быстрой аутентификации без
+            необходимости вводить учетные данные.
+          </li>
+        </ol>
+        <Link href="/" className={styles.link}>
+          на главную
+        </Link>
+      </div>
+    </AuthBlock>
+  );
+}
