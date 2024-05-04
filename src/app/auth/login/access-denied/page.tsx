9c8f7a0d51411ee4d540ce65ab4460e6274684d2
Alexander Berezhnev <freelancerbav@gmail.com>
2024-05-04 22:17:08 +0300

    v3.5.1 A new account is created for each login method.

--- src/app/auth/login/access-denied/page.tsx
+++ src/app/auth/login/access-denied/page.tsx
@@ -7,7 +7,16 @@ export default function AccessDenied() {
   return (
     <AuthBlock>
       <h1 className={styles.title}>Отказ в доступе</h1>
-      Непредвиденная ошибка при аутентификации
+      <div className={styles.text}>
+        Непредвиденная ошибка при аутентификации. Возможные причины:
+        <ul className={styles.list}>
+          <li>не получены данные из провайдера доступа (соцсеть)</li>
+          <li>
+            в системе есть пользователь с таким же email (при входе через разные провайдеры,
+            создаются разные аккаунты и их email могут совпадать)
+          </li>
+        </ul>
+      </div>
       <div>
         <Link href="/" className={styles.link}>
           на главную
