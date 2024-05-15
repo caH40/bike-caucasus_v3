514196f07c604e1908fd3e0f21a01705d5c00378
Alexander Berezhnev <freelancerbav@gmail.com>
2024-05-15 15:45:14 +0300

    v3.13.1 added models for news page

---
+++ src/constants/news.ts
@@ -0,0 +1,6 @@
+export const categories = [
+  { id: 0, translation: 'Анонс', name: 'announcement' },
+  { id: 1, translation: 'Результаты', name: 'results' },
+  { id: 3, translation: 'Событие', name: 'event' },
+  { id: 4, translation: 'Тренировочный заезд', name: 'training_race' },
+];
---
+++ src/database/mongodb/Models/News.ts
@@ -0,0 +1,17 @@
+import { Schema, model, models } from 'mongoose';
+
+// Схема новости
+const NewsSchema = new Schema(
+  {
+    title: { type: String, required: true }, // Заголовок новости.
+    subTitle: { type: String, required: true }, // Кратное описание для отображения поста в ленте.
+    content: { type: String }, // Текст новости.
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Ссылка на автора новости
+    poster: { type: String }, // ССылка на обложку новости.
+    category: { type: String }, // к какой категории относится новость (анонс, результаты и т.д.).
+  },
+  { timestamps: true }
+);
+
+// Модель новости
+export const News = models.News || model('News', NewsSchema);
