c4253b740c83ba7b983b1616ed824282788c2a18
Alexander Berezhnev <freelancerbav@gmail.com>
2024-05-24 09:45:38 +0300

    v3.19.6 Changed footer style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,7 @@ import Link from 'next/link';
 
 import LogoMain from '../UI/LogoMain/LogoMain';
 import styles from './Footer.module.css';
-
-const links1 = [
-  { id: 0, name: 'Главная', href: '/' },
-  { id: 1, name: 'Маршруты', href: '/trails' },
-  { id: 2, name: 'Джилы-Су', href: '/dzhilsu' },
-  { id: 3, name: 'Галерея', href: '/gallery' },
-];
-const links2 = [{ id: 0, name: 'Вебкамеры', href: '/webcam' }];
+import { navLinksFull } from '@/constants/navigation';
 
 const Footer = () => {
   return (
@@ -19,37 +12,38 @@ const Footer = () => {
         <div className={styles.logo}>
           <LogoMain />
         </div>
-        <ul className={styles.list}>
-          {links1.map((link) => (
-            <li className={styles.item} key={link.id}>
-              <Link className={styles.link} href={link.href}>
-                {link.name}
-              </Link>
+
+        {/* Колонка карта сайта, ссылки на все страницы сайта */}
+        <div className={styles.block}>
+          <h4 className={styles.title}>Карта сайта:</h4>
+          <ul className={styles.list}>
+            {navLinksFull.map((link) => (
+              <li className={styles.item} key={link.id}>
+                <Link className={styles.link} href={link.href}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Колонка социальные сети */}
+        <div className={styles.block}>
+          <h4 className={styles.title}>Соцсети:</h4>
+          <ul className={styles.list__social}>
+            <li className={styles.item}>
+              <a href="https://t.me/velokmv">
+                <Image
+                  width={24}
+                  height={24}
+                  className={styles.social__img}
+                  src="/images/icons/telegram.svg"
+                  alt="telegram"
+                />
+              </a>
             </li>
-          ))}
-        </ul>
-        <ul className={styles.list}>
-          {links2.map((link) => (
-            <li className={styles.item} key={link.id}>
-              <Link className={styles.link} href={link.href}>
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className={styles.list__social}>
-          <li className={styles.item}>
-            <a href="https://t.me/velokmv">
-              <Image
-                width={30}
-                height={30}
-                className={styles.social__img}
-                src="/images/icons/telegram.svg"
-                alt="telegram"
-              />
-            </a>
-          </li>
-        </ul>
+          </ul>
+        </div>
       </div>
 
       <div className={styles.copyright}>Copyright © 2022 Bike-Caucasus</div>
