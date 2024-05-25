ff7faf4f979455b8d9540bc7e74052f41b52fe9f
Alexander Berezhnev <freelancerbav@gmail.com>
2024-05-25 10:28:35 +0300

    v3.20.11 Minor fixes.

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -3,7 +3,7 @@ export const navLinksFull = [
   { id: 0, name: 'Главная', href: '/', permission: null },
   { id: 1, name: 'Вебкамеры', href: '/webcam', permission: null },
   // { id: 2, name: 'Маршруты', href: '/trails', permission: null},
-  { id: 3, name: 'Галерея', href: '/gallery', permission: null },
+  // { id: 3, name: 'Галерея', href: '/gallery', permission: null },
   // { id: 4, name: 'Джилы-Су', href: '/dzhilsu', permission: null },
   { id: 5, name: 'Управление', href: '/moderation', permission: 'moderation' },
 ];
