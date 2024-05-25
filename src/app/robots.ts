c5f36d0ec182c0e8c888b2a2a2c553b065e7ad23
Alexander Berezhnev <freelancerbav@gmail.com>
2024-05-25 18:37:02 +0300

    v3.21.3 Robots.txt.

---
+++ src/app/robots.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from 'next';
+
+const host = process.env.NEXT_PUBLIC_SERVER_FRONT || 'https://bike-caucasus.ru';
+
+/**
+ * Генерирует правила для файла robots.txt.
+ * @returns {MetadataRoute.Robots} - Объект с правилами для robots.txt.
+ */
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/moderation',
+          '/moderation/*',
+          '/auth',
+          '/auth/*',
+          '/account',
+          '/account/*',
+        ],
+      },
+    ],
+    sitemap: `${host}/sitemap.xml`,
+  };
+}
