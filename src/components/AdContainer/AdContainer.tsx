398836cb25d51d6a04c315f47f83d329bf2f9c47
Alexander Berezhnev <freelancerbav@gmail.com>
2024-06-10 23:58:54 +0300

    v3.34.3 Minor fixes

--- src/components/AdContainer/AdContainer.tsx
+++ src/components/AdContainer/AdContainer.tsx
@@ -35,7 +35,7 @@ export default function AdContainer({
       style={{ marginBottom, maxHeight, maxWidth, height }}
       id={`yandex_rtb_${adBlock}`}
     >
-      {/* <div className={styles.test} /> */}
+      <div className={styles.test} />
     </div>
   ) : null;
 }
