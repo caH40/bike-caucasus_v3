fd6d6237b8ac67113b46a1914b6a92d6975de50d
Alexander Berezhnev <freelancerbav@gmail.com>
2024-06-18 08:37:31 +0300

    v3.38.5 Disallow pages with ?.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -13,7 +13,8 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: '*',
         allow: '/',
         disallow: [
-          '*?*',
+          '/*?*',
+          '/*?',
           '/moderation',
           '/moderation/*',
           '/auth',
