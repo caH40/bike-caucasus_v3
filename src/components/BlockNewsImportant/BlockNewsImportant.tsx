3224ad9b4b17787016bd1719409a8ec7d6812bd4
Alexander Berezhnev <freelancerbav@gmail.com>
2024-07-12 08:54:19 +0300

    v3.43.14 Minor fixes.

--- src/components/BlockNewsImportant/BlockNewsImportant.tsx
+++ src/components/BlockNewsImportant/BlockNewsImportant.tsx
@@ -11,26 +11,28 @@ import styles from './BlockNewsImportant.module.css';
 export default async function BlockNewsImpotent() {
   const newsLast = await getNews({ docsOnPage: 2, query: { important: true } });
   return (
-    <div className={styles.wrapper}>
-      {newsLast?.news.map((newsOne) => (
-        <Link
-          className={styles.box__poster}
-          href={`/news/${newsOne.urlSlug}`}
-          key={newsOne._id}
-        >
-          <Image
-            src={newsOne.poster}
-            fill={true}
-            sizes="(max-width: 576px 100wv) 33wv"
-            alt={newsOne.title}
-            className={styles.img}
-            priority={true}
-            placeholder="blur"
-            blurDataURL={blurBase64}
-          />
-          <h3 className={styles.title}>{newsOne.title}</h3>
-        </Link>
-      ))}
-    </div>
+    !!newsLast?.news.length && ( // При отсутствии важных новостей не отображать пустой блок.
+      <div className={styles.wrapper}>
+        {newsLast?.news.map((newsOne) => (
+          <Link
+            className={styles.box__poster}
+            href={`/news/${newsOne.urlSlug}`}
+            key={newsOne._id}
+          >
+            <Image
+              src={newsOne.poster}
+              fill={true}
+              sizes="(max-width: 576px 100wv) 33wv"
+              alt={newsOne.title}
+              className={styles.img}
+              priority={true}
+              placeholder="blur"
+              blurDataURL={blurBase64}
+            />
+            <h3 className={styles.title}>{newsOne.title}</h3>
+          </Link>
+        ))}
+      </div>
+    )
   );
 }
