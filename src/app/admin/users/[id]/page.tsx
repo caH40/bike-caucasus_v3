8a0981c39a014c5b41ce996ef542ea5cdca90be1
Alexander Berezhnev <freelancerbav@gmail.com>
2025-06-14 10:23:16 +0300

    v3.71.10 Added a profile picture to the profile editing form by the admin.

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,6 +1,12 @@
+import Image from 'next/image';
+
 import { getRoles } from '@/actions/permissions';
 import { getProfile } from '@/actions/user';
 import { FormModerateUser } from '@/components/UI/Forms/FormModerateUser/FormModerateUser';
+import { blurBase64 } from '@/libs/image';
+
+import styles from '../UsersAdminPage.module.css';
+import { getLogoProfile, getUserFullName } from '@/libs/utils/profile';
 
 export const dynamic = 'force-dynamic';
 
@@ -30,8 +36,29 @@ export default async function UserModeration(props: Props) {
     return <h2>Роли не найдены!</h2>;
   }
 
+  const profileImage = getLogoProfile(
+    profile.data.imageFromProvider,
+    profile.data.provider?.image,
+    profile.data.image
+  );
+
+  const fullName = getUserFullName({
+    person: profile.data.person,
+    showPatronymic: profile.data.preferences.showPatronymic,
+  });
+
   return (
     <div>
+      <Image
+        width={150}
+        height={150}
+        src={profileImage}
+        alt={`Аватар пользователя ${fullName}`}
+        className={styles.profile__image}
+        priority={true}
+        placeholder="blur"
+        blurDataURL={blurBase64}
+      />
       <FormModerateUser profile={profile.data} roles={roles.data} />
     </div>
   );
