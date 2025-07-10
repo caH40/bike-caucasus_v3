840c81fdc5cfe7b00abf5ff9faccf19e723b1139
Alexander Berezhnev <freelancerbav@gmail.com>
2025-07-10 22:37:44 +0300

    v3.75.19 Purchasing slots for creating championships added to the account menu.

--- src/app/account/services-and-finances/competition-slots/page.tsx
+++ src/app/account/services-and-finances/competition-slots/page.tsx
@@ -2,12 +2,17 @@
  * Слоты на соревнования.
  */
 
+import TitleAndLine from '@/components/TitleAndLine/TitleAndLine';
 import UnderConstruction from '@/components/UnderConstruction/UnderConstruction';
 
+/**
+ * Приобретенные слоты на соревнования.
+ */
 export default function CompetitionSlotsPage() {
   return (
     <div>
-      {' '}
+      <TitleAndLine title="Приобретенные слоты на соревнования" hSize={1} />
+
       <UnderConstruction />
     </div>
   );
--- src/app/account/services-and-finances/site-services/page.tsx
+++ src/app/account/services-and-finances/site-services/page.tsx
@@ -1,12 +1,48 @@
-import UnderConstruction from '@/components/UnderConstruction/UnderConstruction';
+import { getServerSession } from 'next-auth';
+
+import ChampionshipSlotPurchasePanel from '@/components/SlotPurchasePanels/ChampionshipSlotPurchasePanel/ChampionshipSlotPurchasePanel';
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options';
+import ServerErrorMessage from '@/components/ServerErrorMessage/ServerErrorMessage';
+import { getAvailableSlots } from '@/actions/slots';
+import { getPriceTier } from '@/actions/price';
+import styles from './SiteServicesPage.module.css';
+import TitleAndLine from '@/components/TitleAndLine/TitleAndLine';
 
 /**
- * Сервисы на сайте.
+ * Приобретенные пользователем сервисы на сайте.
  */
-export default function SiteServicesPage() {
+export default async function SiteServicesPage() {
+  const session = await getServerSession(authOptions);
+
+  const userId = Number(session?.user.id);
+  const userDBId = session?.user.idDB;
+
+  if (!userId || isNaN(userId) || !userDBId) {
+    return <ServerErrorMessage message={'Не получен userId!'} />;
+  }
+
+  const availableSlots = await getAvailableSlots({ entityName: 'championship', userDBId });
+
+  if (!availableSlots.ok || !availableSlots.data) {
+    return <ServerErrorMessage message={availableSlots.message} />;
+  }
+
+  const priceTier = await getPriceTier({ entityName: 'championship' });
+
+  if (!priceTier.ok || !priceTier.data) {
+    return <ServerErrorMessage message={priceTier.message} />;
+  }
+
   return (
-    <div>
-      <UnderConstruction />
+    <div className={styles.wrapper}>
+      <TitleAndLine title="Платны сервисы сайта" hSize={1} />
+
+      <TitleAndLine title="Создание чемпионатов" hSize={2} />
+      <ChampionshipSlotPurchasePanel
+        userId={userId}
+        availableSlots={availableSlots.data.availableSlots}
+        priceTier={priceTier.data.tiers}
+      />
     </div>
   );
 }
