c4ec2ae4cc0fc0a921ac88474e3edc7559c7992e
Alexander Berezhnev <freelancerbav@gmail.com>
2025-07-27 09:00:03 +0300

    v3.76.13 PWA params has been removed.

--- src/meta/meta.ts
+++ src/meta/meta.ts
@@ -50,27 +50,6 @@ export const metadataHomePage = {
     type: 'website',
     images: `./images/og/main.jpg`,
   },
-  icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/icon.svg', sizes: 'any' },
-      { url: '/favicon/favicon-120x120.png', sizes: '120x120', type: 'image/png' },
-      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180' }],
-    other: [
-      {
-        rel: 'manifest',
-        url: '/favicon/site.webmanifest',
-      },
-      {
-        rel: 'mask-icon',
-        url: '/favicon/safari-pinned-tab.svg',
-        color: '#5bbad5',
-      },
-    ],
-  },
 };
 
 /**
